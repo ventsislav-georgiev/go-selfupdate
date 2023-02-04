d25a51d6b3fdc76f587d9618db0cd88c5700b011
ventsislav-georgiev <ventsislav.georgiev@live.com>
2023-02-05 00:03:50 +0200

    feat: ignore name match on gunzip

--- decompress.go
+++ decompress.go
@@ -96,12 +96,6 @@ func gunzip(src io.Reader, cmd, os, arch string) (io.Reader, error) {
 		return nil, fmt.Errorf("%w gzip file: %s", ErrCannotDecompressFile, err)
 	}
 
-	name := r.Header.Name
-	if !matchExecutableName(cmd, os, arch, name) {
-		return nil, fmt.Errorf("%w: expected %q but found %q", ErrExecutableNotFoundInArchive, cmd, name)
-	}
-
-	log.Printf("Executable file %q was found in gzip file", name)
 	return r, nil
 }
 
